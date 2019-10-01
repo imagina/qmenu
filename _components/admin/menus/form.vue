836415b5138eae6d04fa531fb109264128d2b85f
Yeison Tapia <yftapial@ut.edu.co>
2019-10-01 11:59:56 -0500

    + Update components for menu

--- _components/admin/menus/form.vue
+++ _components/admin/menus/form.vue
@@ -1,139 +1,73 @@
 <template>
-  <!--Modal with form to Menu -->
-  <q-modal
-    id="formCategory"
-    v-model="show"
-    v-if="show"
-    no-esc-dismiss
-    no-backdrop-dismiss
-    class="backend-page">
-    <q-modal-layout style="max-width: 1245px">
-      <!--Header-->
-      <q-toolbar slot="header">
-        <q-toolbar-title
-          v-if="!itemId">
-          New Menu
-        </q-toolbar-title>
-        <q-toolbar-title
-          v-else>
-          Update Menu ID: {{itemId}}
-        </q-toolbar-title>
-        <q-btn
-          flat
-          v-close-overlay
-          icon="fas fa-times"/>
-      </q-toolbar>
+  <div class="row gutter-x-sm relative-position" v-if="success">
+  
+    <div class="col-12">
+      <locales v-model="locale" ref="localeComponent" @validate="$v.$touch()"/>
+    </div>
+  
+    <div class="col-12" v-if="locale.success">
+  
+      <q-field
+        class="q-mt-sm"
+        :error="$v.locale.formTemplate.name.$error"
+        :error-label="$tr('ui.message.fieldRequired')">
+        <q-input
+          v-model="locale.formTemplate.name"
+          :stack-label="`${$tr('ui.form.name')}*`"/>
+      </q-field>
+  
+      <q-field
+        class="q-mt-sm"
+        :error="$v.locale.formTemplate.title.$error"
+        :error-label="$tr('ui.message.fieldRequired')">
+        <q-input
+          v-model="locale.formTemplate.title"
+          :stack-label="`${$tr('ui.form.title')} (${locale.language})*`"/>
+      </q-field>
+  
+      <div class="input-title">{{$tr('ui.form.status')}} ({{locale.language}})</div>
+      <tree-select
+        v-model="locale.formTemplate.status"
+        :clearable="true"
+        :options="[
+          {label: this.$tr('ui.label.enabled'), id: 1},
+          {label: this.$tr('ui.label.disabled'), id: 0},
+        ]"
+        placeholder=""/>
+  
+      <div class="input-title">{{$tr('ui.form.default')}}</div>
+      <tree-select
+        v-model="locale.formTemplate.isDefault"
+        :clearable="true"
+        :options="[
+          {label: this.$tr('ui.label.yes'), id: 1},
+          {label: this.$tr('ui.label.no'), id: 0},
+        ]"
+        placeholder=""/>
+  
+      <q-btn
+        class="q-mt-md"
+        v-if="itemId"
+        color="positive" :loading="loading"
+        icon="fas fa-edit" :label="$tr('ui.label.update')" @click="updateItem()"
+        rounded/>
       
-      <!--Footer-->
-      <q-toolbar slot="footer" color="white">
-        <q-toolbar-title>
-        </q-toolbar-title>
-        <!--Button Save-->
-        <q-btn
-          v-if="!itemId"
-          icon="fas fa-save"
-          color="positive"
-          label="Save"
-          :loading="loading"
-          @click="createItem()"/>
-        <!--Button Update-->
-        <q-btn
-          v-else
-          label="Update"
-          icon="fas fa-pen"
-          color="positive"
-          :loading="loading"
-          @click="updateItem()"/>
-      </q-toolbar>
-      
-      <!--Content-->
-      <div class="layout-padding relative-position">
-        <div class="row gutter-x-md">
-          <!--Language-->
-          <div class="col-12">
-            <span
-              class="input-title q-mr-sm">
-              Language
-            </span>
-            <locales
-              v-model="locale"
-              ref="localeComponent"
-              @validate="$v.$touch()"/>
-          </div>
-          
-          <!--Form left (Trasnlatables Fields )-->
-          <div
-            class="col-12 col-md-8"
-            v-if="locale.success">
-            <!--Status -->
-            <div class="input-title">Status ({{locale.language}})*</div>
-            <treeselect
-              :clearable="false"
-              :options="this.status"
-              value-consists-of="BRANCH_PRIORITY"
-              v-model="locale.formTemplate.status"
-              placeholder=""/>
-            <!--Title-->
-            <q-field
-              :error="$v.locale.formTemplate.title.$error"
-              error-label="This field is required">
-              <q-input
-                v-model="locale.formTemplate.title"
-                :stack-label="'Title ('+locale.language+')*'"/>
-            </q-field>
-          </div>
-          <!--Form right( No Trasnlatables Fields) -->
-          <div
-            class="col-12 col-md-4"
-            v-if="locale.success">
-            <!--is_default -->
-            <div class="input-title">Default Menu *</div>
-            <treeselect
-              :clearable="false"
-              :options="this.BoolenaValues"
-              value-consists-of="BRANCH_PRIORITY"
-              v-model="locale.formTemplate.is_default"
-              placeholder=""/>
-            <q-field
-              :error="$v.locale.formTemplate.name.$error"
-              error-label="This field is required">
-              <q-input
-                :stack-label="'Name *'"
-                v-model="locale.formTemplate.name"/>
-            </q-field>
-          </div>
-        </div>
-        <!--Loading-->
-        <inner-loading :visible="loading" />
-      </div>
-    </q-modal-layout>
-  </q-modal>
+    </div>
+    <inner-loading :visible="loading"/>
+  </div>
 </template>
 
 <script>
-  //Services
-  import menuServices from '@imagina/qmenu/_services/index';
-  //Components
-  import Treeselect from '@riophae/vue-treeselect'
-  import '@riophae/vue-treeselect/dist/vue-treeselect.css'
-  import uploadImg from '@imagina/qmedia/_components/form'
   import locales from '@imagina/qsite/_components/locales'
-  import innerLoading from 'src/components/master/innerLoading'
-  //Plugins
-  import {required} from 'vuelidate/lib/validators'
   import _cloneDeep from 'lodash.clonedeep'
+  import {required} from 'vuelidate/lib/validators'
+  
   export default {
-    props: {
-      value: {default: false},
-      itemId: {default: false}
+    components:{
+      locales
     },
-    components: {uploadImg, Treeselect, locales, innerLoading},
     watch: {
-      value(newValue) {
-        this.show = this.value
-      },
-      show(newValue) {
-        this.$emit('input', this.show)
+      $route(to, from) {
         this.initForm()
       }
     },
@@ -147,118 +81,100 @@
     },
     data() {
       return {
-        show: false,
-        locale: _cloneDeep(this.dataLocale),
-        editorText: {},
+        locale: {},
         loading: false,
-        status: [
-          {label:'Active', id:1},
-          {label:'Inactive', id:0}
-        ],
-        BoolenaValues:[
-          {label:'Yes', id:1},
-          {label:'No', id:0}
-        ]
+        success: false,
+        itemId: false,
       }
     },
     computed: {
-      //Data locale component
       dataLocale() {
         return {
           fields: {
             name: '',
-            is_default: null,
+            isDefault: 0,
           },
           fieldsTranslatable: {
             title: '',
-            status: null,
+            status: 1,
           },
           validations: {
-            name: {required},
-            title: {required},
-            is_default: {required},
-            status: {required}
+            title:{ required },
+            name: { required }
           }
         }
       }
     },
     methods: {
-      //Init form
       async initForm() {
         this.loading = true
-        //If ther is Menu Id, request data, else set default data
-        if (this.itemId) await this.getDataItem()
-        else this.locale = _cloneDeep(this.dataLocale)
-        
-        this.$v.$reset()//Reset validations
-        this.show = this.value//Assign props value to show modal
+        this.success = false
+        this.locale = _cloneDeep(this.dataLocale)
+        this.itemId = this.$route.params.id
+        if (this.locale.success) this.$refs.localeComponent.vReset()
+        await this.getData()
+        this.$v.$reset()
+        this.success = true
         this.loading = false
       },
-      //Get data Menu to update
-      getDataItem() {
+      getData() {
         return new Promise((resolve, reject) => {
-          let configName = 'apiRoutes.qmenu.menus'
-          let params = {//Params to request
-            refresh: true,
-            params: {
-              filter: {
-                allTranslations: true
+          const itemId = _cloneDeep(this.itemId)
+          if (itemId) {
+            let configName = 'apiRoutes.qmenu.menus'
+            //Params
+            let params = {
+              refresh: true,
+              params: {
+                filter: {allTranslations: true}
               }
             }
+            //Request
+            this.$crud.show(configName, itemId, params).then(response => {
+              this.orderDataItemToLocale(response.data)
+              resolve(true)//Resolve
+            }).catch(error => {
+              this.$alert.error({message: this.$tr('ui.message.errorRequest'), pos: 'bottom'})
+              this.loading = false
+              reject(false)//Resolve
+            })
+          } else {
+            resolve(true)//Resolve
           }
-          //Request
-          menuServices.crud.show(configName, this.itemId, params).then(response => {
-            this.locale.form = _cloneDeep(response.data)
-            resolve(true)
-          }).catch(error => {
-            this.$helper.alert.error('Failed: ' + error, 'bottom')
-            reject(false)
-          })
         })
       },
-      //Create Menu
-      createItem() {
-        this.$refs.localeComponent.vTouch()//Validate component locales
-        //Check validations
-        if (!this.$v.$error) {
-          this.loading = true
-          let configName = 'apiRoutes.qmenu.menus'
-          menuServices.crud.create(configName, this.locale.form).then(response => {
-            this.$helper.alert.success('Menu created ID: ' + response.data.id)
-            this.initForm()
-            this.loading = false
-            this.show = false
-            this.$emit('created', response.data)
-          }).catch(error => {
-            this.loading = false
-            this.$helper.alert.error('Failed: ' + error)
-          })
-        }else{
-          this.$helper.alert.error('Please check required fields','bottom')
-        }
+      orderDataItemToLocale(data) {
+        let orderData = _cloneDeep(data)
+        this.locale.form = _cloneDeep(orderData)
       },
-      //Update Menu
       updateItem() {
         this.$refs.localeComponent.vTouch()//Validate component locales
         //Check validations
         if (!this.$v.$error) {
           this.loading = true
-          let configname = 'apiRoutes.qmenu.menus'
-          menuServices.crud.update(configname, this.itemId, this.locale.form).then(response => {
-            this.$helper.alert.success('Menu updated ID: ' + this.itemId)
-            this.initForm()
+          let configName = 'apiRoutes.qmenu.menus'
+          this.$crud.update(configName, this.itemId, this.getDataForm()).then(response => {
+            this.$alert.success({message: `${this.$tr('ui.message.recordUpdated')}`})
+            //this.initForm()
             this.loading = false
-            this.show = false
-            this.$emit('updated', response.data)
           }).catch(error => {
             this.loading = false
-            this.$helper.alert.error('Failed: ' + error)
+            this.$alert.error({message: this.$tr('ui.message.recordNoUpdated'), pos: 'bottom'})
           })
-        }else{
-          this.$helper.alert.error('Please check required fields','bottom')
+        } else {
+          this.$alert.error({message: this.$tr('ui.message.formInvalid'), pos: 'bottom'})
         }
       },
-      //Return object to validations
+      getDataForm() {
+        let response = this.locale.form
+        for (var item in response) {
+          let valueItem = response[item]
+          if (valueItem == null || valueItem == undefined)
+            delete response[item]
+        }
+        response.selectable = JSON.stringify(response.selectable)
+        return response
+      },
       getObjectValidation() {
         let response = {}
         if (this.locale && this.locale.formValidations)
@@ -269,6 +185,6 @@
   }
 </script>
 
-<style lang="stylus">
-  @import "~variables";
+<style scoped>
+
 </style>
