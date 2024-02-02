9af48146e949796cd1c45c3b44edb0ffff76f723
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-02-02 17:32:09 -0500

    Fixing styles compilation error

--- _components/admin/menuitems/nested.vue
+++ _components/admin/menuitems/nested.vue
@@ -106,7 +106,9 @@
 </script>
 
 <style lang="scss">
-  .list-group-item
-    .list-group-item
-      padding-left 15px !important
+  .list-group-item {
+    .list-group-item {
+      padding-left: 15px !important;
+    }
+  }
 </style>
