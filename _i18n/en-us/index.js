59ad647ee16f0b31d874a2a8f315ab905d83610c
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-25 12:46:31 -0500

    + added translations

--- _i18n/en-us/index.js
+++ _i18n/en-us/index.js
@@ -1,7 +1,35 @@
-import layout from 'modules/qmenu/_i18n/en-us/layout'
-import sidebar from 'modules/qmenu/_i18n/en-us/sidebar'
-
 export default {
-  layout,
-  sidebar
+  "menu": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Menu",
+        "adminMenus": "Menus",
+        "adminMenuItems": "Menu Items"
+      },
+      "newMenu": "New Menu",
+      "updateMenu": "Update Menu",
+      "newItem": "New Menu Item",
+      "updateItem": "Update Menu Item",
+      "sameTab": "Same Tab",
+      "newTab": "New Tab",
+      "updateOrder": "Update Order",
+      "form": {
+        "url": "URL",
+        "uri": "URI",
+        "class": "CSS class",
+        "target": "Target",
+        "sameTab": "Same Tab",
+        "newTab": "New Tab",
+        "pageName": "Page Name",
+        "typeLink": "Type Link",
+        "page": "Page",
+        "internal": "Page Internal",
+        "external": "Page External",
+        "frontend": "Frontend Page"
+      },
+      "documentation": {
+        "menus": "Your WEB site has one or more minuses that help navigate your WEB site This functionality allows you to manage these menus, modifying positions, elements and texts of each menu"
+      }
+    }
+  }
 }
